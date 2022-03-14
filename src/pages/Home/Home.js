250ad087607bd6880276d314b98289d865d9911a
PriyathamUV <saipriyathamvarma@gmail.com>
2022-03-13 23:16:57 -0400

    question-2::Home page implemented

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,169 @@
+import React, { useState, useEffect, useMemo } from "react"
+import { makeStyles } from "@mui/styles"
+import { useDispatch, useSelector } from "react-redux"
+import { getPosts, removePost } from "../../store/posts"
+import CircularProgress from "@mui/material/CircularProgress"
+import Backdrop from "@mui/material/Backdrop"
+import Card from "@mui/material/Card"
+import CardActions from "@mui/material/CardActions"
+import CardContent from "@mui/material/CardContent"
+import Button from "@mui/material/Button"
+import Typography from "@mui/material/Typography"
+import Paper from "@mui/material/Paper"
+import InputBase from "@mui/material/InputBase"
+import IconButton from "@mui/material/IconButton"
+import SearchIcon from "@mui/icons-material/Search"
+import AddEditModal from "./AddEditModal/AddEditModal"
+
+const useStyles = makeStyles(() => ({
+	root: {
+		padding: "1rem",
+	},
+	posts: {
+		padding: "1rem",
+	},
+	actions: {
+		display: "flex",
+		flexDirection: "row",
+		justifyContent: "space-between",
+		paddingLeft: "1rem",
+		paddingRight: "1rem",
+	},
+}))
+
+const Home = () => {
+	const classes = useStyles()
+	const dispatch = useDispatch()
+	const data = useSelector((store) => store.posts)
+
+	const [open, setOpen] = useState(false)
+	const [postId, setPostId] = useState(0)
+	const [userId, setUserId] = useState("")
+
+	useEffect(() => {
+		dispatch(getPosts())
+	}, [dispatch])
+
+	const handleAdd = () => {
+		setOpen(true)
+		setPostId(0)
+	}
+
+	const handleEdit = (id) => {
+		setOpen(true)
+		setPostId(id)
+	}
+
+	const handleDelete = (id) => {
+		dispatch(removePost(id))
+	}
+
+	const filteredData = useMemo(
+		() =>
+			!!userId
+				? data.posts.filter((record) => record.userId == userId)
+				: data.posts,
+		[data, userId]
+	)
+
+	return (
+		<div className={classes.root}>
+			{data.isLoading ? (
+				<Backdrop
+					sx={{
+						color: "#fff",
+						zIndex: (theme) => theme.zIndex.drawer + 1,
+					}}
+					open={data.isLoading}
+				>
+					<CircularProgress color="inherit" />
+				</Backdrop>
+			) : (
+				<div>
+					<Typography variant="h4" component="div">
+						Posts
+					</Typography>
+					<div className={classes.actions}>
+						<Paper
+							component="form"
+							sx={{
+								p: "2px 4px",
+								display: "flex",
+								alignItems: "center",
+								width: 400,
+							}}
+						>
+							<InputBase
+								sx={{ ml: 1, flex: 1 }}
+								placeholder="Search"
+								inputProps={{ "aria-label": "search" }}
+								value={userId}
+								onChange={(e) => setUserId(e.target.value)}
+							/>
+							<IconButton
+								type="submit"
+								sx={{ p: "10px" }}
+								aria-label="search"
+							>
+								<SearchIcon />
+							</IconButton>
+						</Paper>
+						<Button
+							size="small"
+							variant="contained"
+							color="primary"
+							onClick={handleAdd}
+						>
+							Add Post
+						</Button>
+					</div>
+					{filteredData.map((item) => (
+						<div className={classes.posts}>
+							<Card>
+								<CardContent>
+									<Typography
+										gutterBottom
+										variant="h5"
+										component="div"
+									>
+										{item.title}
+									</Typography>
+									<Typography
+										variant="body2"
+										color="text.secondary"
+									>
+										{item.body}
+									</Typography>
+									<Typography variant="h6">
+										User Id: {item.userId}
+									</Typography>
+								</CardContent>
+								<CardActions>
+									<Button
+										size="small"
+										color="primary"
+										onClick={() => handleEdit(item.id)}
+									>
+										Edit
+									</Button>
+									<Button
+										size="small"
+										color="warning"
+										onClick={() => handleDelete(item.id)}
+									>
+										Delete
+									</Button>
+								</CardActions>
+							</Card>
+						</div>
+					))}
+				</div>
+			)}
+			{open && (
+				<AddEditModal open={open} setOpen={setOpen} postId={postId} />
+			)}
+		</div>
+	)
+}
+
+export default Home
--- src/store/posts.js
+++ src/store/posts.js
@@ -4,33 +4,120 @@ import * as apiService from "../services/ApiService"
 const actionTypes = {
 	RQT_POSTS: "RQT_POSTS",
 	RCV_POSTS: "RCV_POSTS",
+	RQT_ADD_POST: "RQT_ADD_POST",
+	RCV_ADD_POST: "RCV_ADD_POST",
+	RQT_EDIT_POST: "RQT_EDIT_POST",
+	RCV_EDIT_POST: "RCV_EDIT_POST",
+	RQT_REMOVE_POST: "RQT_REMOVE_POST",
+	RCV_REMOVE_POST: "RCV_REMOVE_POST",
 }
 
 export const getPosts = () => ({ type: actionTypes.RQT_POSTS })
 
+export const addPost = (payload) => ({
+	type: actionTypes.RQT_ADD_POST,
+	payload,
+})
+export const editPost = (payload) => ({
+	type: actionTypes.RQT_EDIT_POST,
+	payload,
+})
+export const removePost = (payload) => ({
+	type: actionTypes.RQT_REMOVE_POST,
+	payload,
+})
+
 const initialState = {
 	posts: [],
+	isLoading: false,
 }
 
 export default function posts(state = initialState, action) {
+	console.log(state, action)
 	switch (action.type) {
 		case actionTypes.RCV_POSTS:
 			return {
 				...state,
+				isLoading: false,
 				posts: action.posts,
+			}
+		case actionTypes.RQT_POSTS:
+			return {
+				...state,
+				isLoading: true,
+			}
+		case actionTypes.RCV_ADD_POST:
+			return {
+				...state,
+				posts: [action.response, ...state.posts],
+			}
+		case actionTypes.RCV_EDIT_POST:
+			const index = state.posts.findIndex(
+				(item) => item.id === action.response.id
+			)
+			const data = [...state.posts]
+			data[index] = action.response
+			return {
+				...state,
+				posts: [...data],
+			}
+		case actionTypes.RCV_REMOVE_POST:
+			const posts = state.posts.filter((item) => item.id !== action.id)
+			return {
+				...state,
+				posts: [...posts],
 			}
 		default:
 			return state
 	}
 }
 
-function* fetchPosts() {
-	try {
-		const posts = yield call(apiService.getPosts)
-		yield put({ type: "RCV_POSTS", posts })
-	} catch (e) {
-		console.log(e)
-	}
-}
 
-export const postsSagas = all([takeLatest(actionTypes.RQT_POSTS, fetchPosts)])
+	function* readPosts() {
+		try {
+			const posts = yield call(apiService.getPosts)
+			yield put({ type: "RCV_POSTS", posts })
+		} catch (e) {
+			console.log(e)
+		}
+	}
+	
+	function* createPost(data) {
+		console.log(data)
+		try {
+			const response = yield call(apiService.addPost, data.payload)
+			console.log(response)
+			yield put({ type: "RCV_ADD_POST", response })
+		} catch (e) {
+			console.log(e)
+		}
+	}
+	
+	function* updatePost(data) {
+		try {
+			const response = yield call(apiService.editPost, data.payload)
+			console.log(response)
+			yield put({ type: "RCV_EDIT_POST", response })
+		} catch (e) {
+			console.log(e)
+		}
+	}
+	
+	function* deletePost(data) {
+		console.log(data)
+		try {
+			yield call(apiService.removePost, data.payload)
+			yield put({ type: "RCV_REMOVE_POST", id: data.payload })
+		} catch (e) {
+			console.log(e)
+		}
+	}
+	
+
+
+export const postsSagas = all([
+	takeLatest(actionTypes.RQT_ADD_POST, createPost),
+	takeLatest(actionTypes.RQT_POSTS, readPosts),
+	takeLatest(actionTypes.RQT_EDIT_POST, updatePost),
+	takeLatest(actionTypes.RQT_REMOVE_POST, deletePost),
+])
